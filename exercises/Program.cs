fe43e6cdd5ca3029d8981167e7fd7c0e282369b5
Roman <roman.rakov2909@gmail.com>
2023-10-05 23:42:29 +0300

    Добавьте файлы проекта.

---
+++ exercises/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Threading;
+
+namespace exercises
+{
+    internal class Program
+    {
+        enum level
+        {
+            Low,
+            Medium,
+            Hight
+        }
+        struct Grandfather
+        {
+            public string name;
+            public level grouchinesslevel;
+            public string[] grumblingphrases;
+            public int bruises;
+            public void Countsbruises(params string[] cursewords)
+            {
+                foreach (string elem in cursewords)
+                {
+                    foreach (string element in grumblingphrases)
+                    {
+                        if (elem == element)
+                        {
+                            bruises++;
+                        }
+                    }
+                }
+            }
+        }
+        static int Sum(params int[] paraments)
+        {
+            int sum = 0;
+            for (int i = 0; i < paraments.Length; i++)
+            {
+                sum += paraments[i];
+            }
+            return sum;
+        }
+        static void Comp(int[] paraments, ref int comp)
+        {
+            comp = 1;
+            for (int i = 0; i < paraments.Length; i++)
+            {
+                comp *= paraments[i];
+            }
+        }
+        static void Average(int[] paraments, out double average)
+        {
+            double sum = 0;
+            for (int i = 0; i < paraments.Length; i++)
+            {
+                sum += paraments[i];
+            }
+            average = sum / paraments.Length;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 1\nПоменять 2 числа из массива местами");
+            Random rnd = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i <= 19; i++)
+            {
+                array[i] = rnd.Next(99999);
+            }
+            for (int i = 0; i <= 19; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\nВведите 1 число:");
+            int number1, number2, number3, number4;
+
+            int.TryParse(Console.ReadLine(), out number1);
+            Console.WriteLine("Введите 2 число:");
+            int.TryParse(Console.ReadLine(), out number2);
+            for (int i = 0; i <= 19; i++)
+            {
+                number3 = number1;
+                number4 = number2;
+                if (array[i] == number3)
+                {
+                    array[i] = number2;
+                }
+                else if (array[i] == number4)
+                {
+                    array[i] = number1;
+                }
+            }
+            for (int i = 0; i <= 19; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine("\n\nУпражнение 2\nВывести среднее арифметическое, сумму и произведение массива");
+            Random rnd1 = new Random();
+            int[] array1 = new int[5];
+            int sum = 0;
+            int comp = 1;
+            double average;
+            for (int i = 0; i <= 4; i++)
+            {
+                array1[i] = rnd.Next(99);
+                Console.Write(array1[i] + " ");
+            }
+            sum = Sum(array1);
+            Console.WriteLine($"\nCумма равна = {sum}");
+            Comp(array1, ref comp);
+            Console.WriteLine($"Произведение равно = {comp}");
+            Average(array1, out average);
+            Console.WriteLine($"Среднее арифметическое равно = {average}\n");
+
+            Console.WriteLine("Упражнение 3\nВведите число от 0 до 9, чтобы получить рисунок или exit/закрыть для завершения работы программы:");
+            string some = Console.ReadLine();
+            if (some == "exit" || some == "закрыть")
+            {
+                System.Environment.Exit(0);
+            }
+            try
+            {
+                int num = Convert.ToInt32(some);
+                switch (num)
+                {
+                    case 0:
+                        Console.WriteLine("###\n# # \n###");
+                        break;
+                    case 1:
+                        Console.WriteLine(" # \n # \n #\n # ");
+                        break;
+                    case 2:
+                        Console.WriteLine("###\n  #\n #  \n###");
+                        break;
+                    case 3:
+                        Console.WriteLine("###\n  #\n###\n  #\n###");
+                        break;
+                    case 4:
+                        Console.WriteLine("# #\n# #\n  #");
+                        break;
+                    case 5:
+                        Console.WriteLine("###\n#  \n###\n  #\n###");
+                        break;
+                    case 6:
+                        Console.WriteLine("###\n#  \n###\n# #\n###");
+                        break;
+                    case 7:
+                        Console.WriteLine("###\n  #\n #\n#  \n#  ");
+                        break;
+                    case 8:
+                        Console.WriteLine("###\n# #\n###\n# #\n###");
+                        break;
+                    case 9:
+                        Console.WriteLine("###\n# #\n###\n  #\n###");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Число должно быть от 0 до 9 \n");
+                        Thread.Sleep(5000);
+                        Console.ResetColor();
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Вы должны ввести число");
+            }
+
+            Console.WriteLine("\nУпражнение 4\nСоздать структуру Дед и вернуть количество фингалов");
+            string[] cursewords = {"Проститутки", "Гады"};
+            string[] васяwords = {"Проститутки", "Дебилы"};
+            string[] коляwords = {"Гады", "Уроды", "Дурак"};
+            string[] ваняwords = {"Мразь"};
+            string[] витяwords = {"Огрызок", "Рахитоид ", "Огузок", "Инвалид"};
+            string[] сашаwords = {"Проститутки", "Гады", "Дебилы", "Твари", "Уебки", "Ебланы"};
+            Grandfather Вася = new Grandfather();
+            Вася.name = "Вася";
+            level ВасяgrouchinessLevel = level.Low;
+            Вася.grouchinesslevel = ВасяgrouchinessLevel;
+            Вася.grumblingphrases = васяwords;
+            Вася.bruises = 0;
+            Вася.Countsbruises(cursewords);
+            Grandfather Коля = new Grandfather();
+            Коля.name = "Коля";
+            level КоляgrouchinessLevel = level.Medium;
+            Коля.grouchinesslevel = КоляgrouchinessLevel;
+            Коля.grumblingphrases = коляwords;
+            Коля.bruises = 0;
+            Коля.Countsbruises(cursewords);
+            Grandfather Ваня = new Grandfather();
+            Ваня.name = "Ваня";
+            level ВаняgrouchinessLevel = level.Medium;
+            Ваня.grouchinesslevel = ВаняgrouchinessLevel;
+            Ваня.grumblingphrases = ваняwords;
+            Ваня.bruises = 0;
+            Ваня.Countsbruises(cursewords);
+            Grandfather Витя = new Grandfather();
+            Витя.name = "Витя";
+            level ВитяgrouchinessLevel = level.Hight;
+            Витя.grouchinesslevel = ВитяgrouchinessLevel;
+            Витя.grumblingphrases = витяwords;
+            Витя.bruises = 0;
+            Витя.Countsbruises(cursewords);
+            Grandfather Саша = new Grandfather();
+            Саша.name = "Саша";
+            level СашаgrouchinessLevel = level.Hight;
+            Саша.grouchinesslevel = СашаgrouchinessLevel;
+            Саша.grumblingphrases = сашаwords;
+            Саша.bruises = 0;
+            Саша.Countsbruises(cursewords);
+            Console.WriteLine($"Количество синяков у Васи: {Вася.bruises}\n" +
+                              $"Количество синяков у Коли: {Коля.bruises}\n" +
+                              $"Количество синяков у Вани: {Ваня.bruises}\n" +
+                              $"Количество синяков у Вити: {Витя.bruises}\n" +
+                              $"Количество синяков у Саши: {Саша.bruises}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab5/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace lab5
+{
+    class Program
+    {
+        static int max(int newnumber1, int newnumber2)
+        {
+            if (newnumber1 > newnumber2)
+            {
+                return newnumber1;
+            }
+            else
+            {
+                return newnumber2;
+            }
+        }
+        static void swapper(ref string firstnumber, ref string secondnumber)
+        {
+            string thirdnumber;
+            thirdnumber = firstnumber;
+            firstnumber = secondnumber;
+            secondnumber = thirdnumber;
+        }
+        static void factorial(int value)
+        {
+            int fact = 1;
+            try
+            {
+                for (int i = 1; i <= value; i++)
+                {
+                    checked
+                    {
+                        fact = fact * i;
+                    }
+                }
+                bool result = true;
+                Console.WriteLine(result);
+
+            }
+            catch
+            {
+                bool result = false;
+                Console.WriteLine(result);
+            }
+        }
+        static int Factorial(int newvalue)
+        {
+            if (newvalue == 1)
+            {
+                newvalue = 1;
+                return newvalue;
+            }
+            else
+            {
+                return newvalue * Factorial(newvalue - 1);
+            }
+        }
+        static int NOD(int newvalue1, int newvalue2)
+        {
+            while (newvalue1 != newvalue2)
+            {
+                if (newvalue1 > newvalue2)
+                {
+                    newvalue1 = newvalue1 - newvalue2;
+                }
+                else
+                {
+                    newvalue2 = newvalue2 - newvalue1;
+                }
+            }
+            return (newvalue1);
+
+        }
+        static int NOD(int newvalue1, int newvalue2, int newvalue3)
+        {
+            int temporarynod = NOD(newvalue1, newvalue2);
+            int Nod = NOD(temporarynod, newvalue3);
+            return Nod;
+        }
+        static int Fibonachi(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+            else
+            {
+                return Fibonachi(number - 1) + Fibonachi(number - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 5.1\nНаписать метод, возвращающий наибольшее из двух чисел\nВведите 1 число:");
+            int newnumber1, newnumber2;
+            string number1 = Console.ReadLine();
+            int.TryParse(number1, out newnumber1);
+            Console.WriteLine("Введите 2 число:");
+            string number2 = Console.ReadLine();
+            int.TryParse(number2, out newnumber2);
+            int Max = max(newnumber1, newnumber2);
+            Console.WriteLine($"Максимальное значение = {Max}\n");
+
+            Console.WriteLine("Упражнение 5.2\nНаписать метод, который меняет местами значения двух передаваемых параметров\nВведите 1 число:");
+            string firstnumber = Console.ReadLine();
+            Console.WriteLine("Введите 2 число:");
+            string secondnumber = Console.ReadLine();
+            swapper(ref firstnumber, ref secondnumber);
+            Console.WriteLine($"Новое значение первого числа = {firstnumber}\nНовое значение второго числа = {secondnumber}\n");
+
+            Console.WriteLine("Упражнение 5.3\nНаписать метод вычисления факториала числа\nВведите число:");
+            int value;
+            int.TryParse(Console.ReadLine(), out value);
+            factorial(value);
+
+            Console.WriteLine("Упражнение 5.4\nНаписать рекурсивный метод вычисления факториала числа.\nВведите число:");
+            int newvalue;
+            int.TryParse(Console.ReadLine(), out newvalue);
+            int fact = Factorial(newvalue);
+            Console.WriteLine($"Факториал = {fact}\n");
+
+            Console.WriteLine("Домашнее задание 5.1\nНаписать метод, который вычисляет НОД для 2 чисел\nВведите 1 число:");
+            int newvalue1, newvalue2;
+            string value1 = Console.ReadLine();
+            int.TryParse(value1, out newvalue1);
+            Console.WriteLine("Введите 2 число:");
+            string value2 = Console.ReadLine();
+            int.TryParse(value2, out newvalue2);
+            int nod = NOD(newvalue1, newvalue2);
+            Console.WriteLine($"НОД = {nod}\n");
+
+            Console.WriteLine("Написать метод, который вычисляет НОД для 3 чисел\nВведите 1 число:");
+            int firstvalue1, secondvalue2, thirdvalue3;
+            string firstvalue = Console.ReadLine();
+            int.TryParse(firstvalue, out firstvalue1);
+            Console.WriteLine("Введите 2 число:");
+            string secondvalue = Console.ReadLine();
+            int.TryParse(secondvalue, out secondvalue2);
+            Console.WriteLine("Введите 3 число:");
+            string thirdvalue = Console.ReadLine();
+            int.TryParse(thirdvalue, out thirdvalue3);
+            int Nod = NOD(firstvalue1, secondvalue2, thirdvalue3);
+            Console.WriteLine($"НОД = {Nod}\n");
+
+            Console.WriteLine("Домашнее задание 5.2\nНаписать рекурсивный метод, вычисляющий значение n-го числа ряда Фибоначчи\nВведите число:");
+            int number;
+            int.TryParse(Console.ReadLine(), out number);
+            int fib = Fibonachi(number);
+            Console.WriteLine($"Значение n-го числа = {fib}");
+            Console.ReadKey();
+        }
+    }
+}
